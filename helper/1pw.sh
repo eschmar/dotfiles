0f57df9b91e73993ac7c9837c8d2b21c993bacbb
Marcel Eschmann <marcel.eschmann@gmail.com>
2018-11-05 14:42:18 +0100

    Fix 1pw helper script

--- helper/1pw.sh
+++ helper/1pw.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cp /Users/eschmar/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/2bua8c4s2c.com.agilebits.1password.json /Users/eschmar/Library/Application\ Support/Google/Chrome\ Canary/
+mkdir -p /Users/$USER/Library/Application\ Support/Google/Chrome\ Canary/NativeMessagingHosts/
+cp /Users/$USER/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/2bua8c4s2c.com.agilebits.1password.json /Users/$USER/Library/Application\ Support/Google/Chrome\ Canary/NativeMessagingHosts/
