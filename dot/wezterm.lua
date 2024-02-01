85ae2c02adebf3a53eb666f5a41a23a36e4255c5
Marcel Eschmann <marcel.eschmann@gmail.com>
2024-02-01 17:54:02 +0100

    Add wezterm

---
+++ dot/wezterm.lua
@@ -0,0 +1,29 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.font_size = 13.0
+
+config.color_scheme = "Cobalt2"
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
+config.use_fancy_tab_bar = false
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
