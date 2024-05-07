758bb1a6fe2477b28ba7c971cf160cb97368f9ff
Marcel Eschmann <marcel.eschmann@gmail.com>
2024-05-07 12:25:38 +0200

    Add ghostty

--- helper/dotfiles.sh
+++ helper/dotfiles.sh
@@ -3,6 +3,9 @@ dir=~/dotfiles/dot
 files="bash_profile zshenv nanorc alias vimrc env wezterm.lua"
 
 for file in $files; do
-    rm -f ~/.$file
-    ln -s $dir/$file ~/.$file
+	rm -f ~/.$file
+	ln -s $dir/$file ~/.$file
 done
+
+mkdir -p ~/.config/ghostty
+ln -s ~/dotfiles/ghostty/config ~/.config/ghostty/config
